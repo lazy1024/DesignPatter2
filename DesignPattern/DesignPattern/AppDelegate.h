c01fbec64bcf7af942e2f24265d5b98ab3fb7fe7
lazy <lazy10240@126.com>
2016-07-14 20:56:38 +0800

    DesignPattern2.0

---
+++ DesignPattern/DesignPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignPattern
+//
+//  Created by magnimage on 16/7/14.
+//  Copyright © 2016年 Lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignPattern/DesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPattern
+//
+//  Created by magnimage on 16/7/14.
+//  Copyright © 2016年 Lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
